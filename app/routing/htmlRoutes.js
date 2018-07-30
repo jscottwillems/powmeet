0a623d5c585dfd70dd93a8a8b8628d1676da4f4c
Josh Willems <jscottwillems@gmail.com>
2018-07-30 11:53:40 -0500

    attempt to deploy

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var path = require("path");
+var server = require("../../server");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/survey", function(req, res) {
