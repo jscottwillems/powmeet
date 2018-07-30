1372456afd09cd3626a3bad34d984b9f28edbfe4
Josh Willems <jscottwillems@gmail.com>
2018-07-30 18:08:06 -0500

    fixed file linking

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,11 @@
-var express = require("express");
-var path = require("path");
-var server = require("../../server");
+module.exports = function (app) {
+    var path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "index.html"));
+    });
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"))
-});
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"))
+    });
+}
